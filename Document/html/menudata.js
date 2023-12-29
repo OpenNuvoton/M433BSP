f23c55290d41bd7b068d2c5d10da9bf66362f728
Jacky Huang <ychuang3@nuvoton.com>
2023-12-29 12:14:48 +0800

    Update readme and driver reference document
    Change-Id: I4b4e93d753e2360b6d6f49bc7a4370a54ce7e439

--- Document/html/menudata.js
+++ Document/html/menudata.js
@@ -103,8 +103,7 @@ var menudata={children:[
 {text:"t",url:"globals_t.html#index_t"},
 {text:"u",url:"globals_u.html#index_u"},
 {text:"v",url:"globals_v.html#index_v"},
-{text:"w",url:"globals_w.html#index_w"},
-{text:"x",url:"globals_x.html#index_x"}]},
+{text:"w",url:"globals_w.html#index_w"}]},
 {text:"Functions",url:"globals_func.html",children:[
 {text:"_",url:"globals_func.html#index__5F"},
 {text:"a",url:"globals_func_a.html#index_a"},
@@ -179,5 +178,4 @@ var menudata={children:[
 {text:"s",url:"globals_defs_s.html#index_s"},
 {text:"t",url:"globals_defs_t.html#index_t"},
 {text:"u",url:"globals_defs_u.html#index_u"},
-{text:"w",url:"globals_defs_w.html#index_w"},
-{text:"x",url:"globals_defs_x.html#index_x"}]}]}]}]}
+{text:"w",url:"globals_defs_w.html#index_w"}]}]}]}]}
